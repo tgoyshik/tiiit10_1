1ad7050e1ef8d59367fb9fdf466cd8fcb6bcb93b
Timofey <bytimofey@gmail.com>
2025-05-12 22:01:37 +0300

    Revert "Исправил root.cpp"
    This reverts commit 67597bae2a090150151d30b61972afc12c224cff.

--- root.cpp
+++ root.cpp
@@ -8,26 +8,25 @@ int main() {
     double a, b, c;
     double discriminant;
     double root1, root2;
-
+    
     cout << "Enter coefficients a, b and c: ";
-    cin >> a >> b >> c;
-
-    discriminant = b * b - 4 * a * c;
-
-    if (discriminant > 0) {
-        root1 = (-b + sqrt(discriminant)) / (2 * a);
-        root2 = (-b - sqrt(discriminant)) / (2 * a);
+    cin >> a >> b >> c
+    
+    discriminant = b*b - 4*a*c;
+    
+    if (dscriminant > 0) {
+        root1 = (-b + sqrt(discriminant)) / (2*a);
+        root2 = (-b - sqrt(discriminant)) / (2*a);
         cout << fixed << setprecision(4);
         cout << "Root 1 = " << root1 << endl;
         cout << "Root 2 = " << root2 << endl;
     }
     else if (discriminant == 0) {
-        root1 = -b / (2 * a);
+        root1 = -b/(2*a);
         cout << fixed << setprecision(4);
         cout << "Root = " << root1 << endl;
     }
     else {
         cout << "No real roots." << endl;
     }
-    return 0;
 }
